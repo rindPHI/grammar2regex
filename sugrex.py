62df2571733586f31d2139d43062dd5c9bedba2e
Dominic Steinhöfel <dominic.steinhoefel@cispa.de>
2021-03-31 15:44:04 +0200

    Regex conversion of trees

--- sugrex.py
+++ sugrex.py
@@ -1,13 +1,16 @@
 import copy
+from functools import reduce
+
 import itertools
 import re
 from enum import Enum
 from typing import Dict, List, Tuple, Set, Union, Callable
 
 import pydot
+import z3
 from fuzzingbook.Grammars import unreachable_nonterminals, nonterminals, is_nonterminal
 
-from grammargraph import GrammarGraph, NonterminalNode, ChoiceNode, TerminalNode
+from grammargraph import GrammarGraph, NonterminalNode, ChoiceNode, TerminalNode, Node
 
 Nonterminal = str
 Grammar = Dict[Nonterminal, List[str]]
@@ -15,9 +18,8 @@ Grammar = Dict[Nonterminal, List[str]]
 
 class GrammarType(Enum):
     UNDET = 0  # Yet unknown
-    NON_RECURSIVE = 1  # Expansions form a non-recursive "call tree" without backlinks
-    LEFT_LINEAR = 2
-    RIGHT_LINEAR = 3
+    LEFT_LINEAR = 1
+    RIGHT_LINEAR = 2
 
 
 class RegularityChecker:
@@ -26,23 +28,55 @@ class RegularityChecker:
         self.grammar_type = GrammarType.UNDET
         self.grammar_graph = GrammarGraph.from_grammar(grammar)
 
+    def regular_expression_from_tree(self, node: Union[str, Node]) -> z3.ReRef:
+        if type(node) is str:
+            node = self.grammar_graph.get_node(node)
+            assert node
+
+        if type(node) is TerminalNode:
+            return z3.Re(node.symbol)
+
+        assert type(node) is NonterminalNode
+        assert self.grammar_graph.subgraph(node).is_tree()
+
+        node: NonterminalNode
+        choice_node: ChoiceNode
+
+        result: Union[z3.ReRef, None] = None
+        for choice_node in node.children:
+            children_regexes = [self.regular_expression_from_tree(child) for child in choice_node.children]
+            if len(children_regexes) == 1:
+                child_result = children_regexes[0]
+            else:
+                child_result = z3.Concat(*children_regexes)
+
+            if result is None:
+                result = child_result
+            else:
+                result = z3.Union(result, child_result)
+
+        assert result is not None
+        return result
+
+
+
     def is_regular(self, nonterminal: Union[str, NonterminalNode], call_seq: Tuple = ()) -> bool:
         """
         Checks whether the language generated from "grammar" starting in "nonterminal" is regular.
-        This is the case if
-
-        1. all productions either are left- or rightlinear, or
-        2. refer to nonterminals constructing a regular language. There must be no back-references from those.
+        This is the case if all productions either are left- or rightlinear, with the exception that
+        they may contain nonterminals that form a tree in the grammar. Those trivially represent
+        retular expressions.
 
         :param grammar: A context-free grammar.
         :param nonterminal: A nonterminal within this grammar.
+        :param call_seq: A tuple of already seen nodes, used to break infinite recursion.
         :return: True iff the language defined by the sublanguage of grammar's language when
         starting in nonterminal is regular.
         """
 
-
         if type(nonterminal) is str:
             nonterminal = self.grammar_graph.get_node(nonterminal)
+            assert nonterminal
         nonterminal: NonterminalNode
 
         if nonterminal in call_seq:
@@ -98,8 +132,3 @@ class RegularityChecker:
                     all_nontree_children_nonterminals.add(child)
 
         return all([self.is_regular(child, call_seq + (nonterminal,)) for child in all_nontree_children_nonterminals])
-
-    @staticmethod
-    def delete_unreachable(grammar: Grammar):
-        for unreachable in unreachable_nonterminals(grammar):
-            del grammar[unreachable]
